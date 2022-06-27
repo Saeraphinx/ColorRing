55a99191452f1bad61236eeb0f9b0d958d00d3d3
Saeraphinx <saeraphinx@gmail.com>
2022-06-27 18:55:21 -0500

    add a singluar comment

--- WebFiles/script.js
+++ WebFiles/script.js
@@ -1,6 +1,7 @@
 function brightness() {
     var brightness = document.getElementById("in_bright").value;
 
+    //validate input
     if(brightness > 255) {
         brightness = 255;
     } else if(brightness < 0) {
